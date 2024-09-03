145d68ac7b159716d4379496b155c8c862e5480c
winter <sohee@weconec.com>
2024-09-03 10:19:34 +0900

    경로수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import Carousel from "../components/home/Carousel";
 import Weather from "../components/home/Weather";
 import Banner from "../components/home/Banner";
 import { useAtom } from "jotai";
-import { userIsMobileAtom } from "src/hooks/atom/searchFilter";
+import { userIsMobileAtom } from "../hooks/atom/searchFilter";
 
 const Home = () => {
   const [isWebView, setIsWebView] = useAtom(userIsMobileAtom);
